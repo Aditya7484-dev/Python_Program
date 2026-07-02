a01c39f0a9cd72c3157d830edc6141a14b4ab6a6
Aditya Kumar <Kumaraditya3555@gmail.com>
2026-07-02 14:42:51 +0530

    Returning to python

--- Mega Project_1_Python/musicList.py
+++
@@ -1,9 +0,0 @@
-music = {
-  "payal":"https://www.youtube.com/watch?v=a-PAcmi5Kas&pp=ygUFcGF5YWw%3D",
-  
-  "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo&pp=ygUHc2t5ZmFsbA%3D%3D",
-  
-  "routine":"https://www.youtube.com/watch?v=Mw9U7FPaZho&pp=ygUYcm91dGluZSBzb25nIGFsYW4gd2Fsa2Vy",
-  
-  "rhinestone eyes":"https://www.youtube.com/watch?v=yYDmaexVHic&pp=ygUPcmhpbmVzdG9uZSBleWVz"
-  }
--- Mega_Project_2_Python/Coordinates.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-while True:
-  a=pyautogui.position()
-  print(a)
-  time.sleep(1)
-  # 536,1049
